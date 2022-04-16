4e965783d5f7965a302353eaf7e8284f47c1eb13
Melinnaaa <arturo.reyes.c01@mail.pucv.cl>
2022-04-16 14:47:22 -0400

    Se agrega la función que ordena los productos y la funcion is equal que estaba mal implementada

--- main.c
+++ main.c
@@ -4,16 +4,24 @@
 #include "./Carpeta/Map.h"
 #include "./Carpeta/producto.h"
 
-int is_equal_int(void * key1, void * key2) {
-    if(*(int*)key1 == *(int*)key2) return 1;
+int is_equal_string(void * key1, void * key2) {
+    if(strcmp((char*)key1, (char*)key2)==0) return 1;
+    return 0;
+}
+
+int lower_than_string(void * key1, void * key2) {
+    if(strcmp((char*)key1, (char*)key2) < 0) return 1;
     return 0;
 }
 
 int main (void)
 {
-    Map* mapaTipo = createMap(is_equal_int); // 20 es la capacidad momentanea que tendra el mapa
-    Map* mapaNombre = createMap(is_equal_int);
-    Map* mapaMarca = createMap(is_equal_int);
+    Map* mapaTipo = createMap(is_equal_string);
+    setSortFunction(mapaTipo,lower_than_string);
+    Map* mapaNombre = createMap(is_equal_string);
+    setSortFunction(mapaNombre,lower_than_string);
+    Map* mapaMarca = createMap(is_equal_string);
+    setSortFunction(mapaMarca,lower_than_string);
     menu(mapaTipo, mapaNombre, mapaMarca);
     return (0);
 }
